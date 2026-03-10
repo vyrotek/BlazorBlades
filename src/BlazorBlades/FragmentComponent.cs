b2c414ad8f357906f5eb8b2dddd9166658b1c01a
Jason <jason@vyrotek.com>
2026-03-09 21:55:04 -0700

    Fragments and more demo examples

---
+++ src/BlazorBlades/FragmentComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorBlades
+{
+    internal class FragmentComponent : ComponentBase
+    {
+        [Parameter]
+        public required RenderFragment RenderFragment { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.AddContent(0, RenderFragment);
+        }
+    }
+}
---
+++ src/BlazorBlades/ResultsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BlazorBlades;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Microsoft.AspNetCore.Http
+{
+    public static class ResultsExtensions
+    {
+        extension(Results)
+        {
+            public static RazorComponentResult Razor(RenderFragment fragment, string? contentType = null, int? statusCode = null)
+            {
+                return new RazorComponentResult<FragmentComponent>(new Dictionary<string, object?>
+                {
+                    [nameof(FragmentComponent.RenderFragment)] = fragment
+                })
+                {
+                    PreventStreamingRendering = true,
+                    ContentType = contentType,
+                    StatusCode = statusCode
+                };
+            }
+        }
+    }
+}
